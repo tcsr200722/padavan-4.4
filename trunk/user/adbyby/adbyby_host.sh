abb8de585786bf1ed38069d42c58b8ae595fbb02
tcsr200722 <350524166@qq.com>
2023-11-27 15:51:20 +0800

    Update adbyby_host.sh

--- trunk/user/adbyby/adbyby_host.sh
+++ trunk/user/adbyby/adbyby_host.sh
@@ -1,5 +1,12 @@
 #一行一个地址，不需要的在前面加 "#"号注释，后台会自动下载，合并，去重。不建议太多，以免误杀严重！
 #此hosts规则是加载到dnsmasq的。
-#yhost
-https://cdn.jsdelivr.net/gh/VeleSila/yhosts/hosts
-
+#https://cdn.jsdelivr.net/gh/VeleSila/yhosts/hosts
+#https://dev.tencent.com/u/shaoxia1991/p/yhosts/git/raw/master/data/tvbox.txt
+#https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad-pc.txt
+#https://gitea.com/Cats-Team/AdRules/raw/branch/main/hosts.txt
+#https://easylist-downloads.adblockplus.org/easylist.txt
+#https://easylist-downloads.adblockplus.org/easylistchina.txt
+#https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt
+#https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
+#https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad-pc.txt
+#https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad3.txt
